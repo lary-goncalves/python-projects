40cac0d6c4f7efa1a9b8135073227ed6913330e7
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-06-14 21:38:38 -0300

    Update ex064.py

--- Phython-Exercise/ex064.py
+++ Phython-Exercise/ex064.py
@@ -1,9 +1,7 @@
-soma = 0
-cont = 0
-num = 0
+soma = cont = num = 0
+num = int(input('Digite um número [999 para parar]: '))
 while num !=999:
-    num = int(input('Digite um número [999 para parar]: '))
     soma+=num
     cont += 1
-
-print(f'Você digitou {cont-1} números e a soma entre foi {soma-999}')
+    num = int(input('Digite um número [999 para parar]: '))
+print(f'Você digitou {cont} números e a soma entre foi {soma}')
