da6233eac2f218341e058477c57b78bb4df4fae7
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-06-21 23:12:53 -0300

    Exercício 070 Finalizado

---
+++ Phython-Exercise/ex070.py
@@ -0,0 +1,30 @@
+total = cont = menor = 0
+NomeMenor = ' '
+print('-'*50)
+print(' '*15,'LOJA SUPER BARATÃO')
+print('-'*50)
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: '))
+    total += preco
+    if menor == 0 and NomeMenor == ' ':
+        menor = preco
+        NomeMenor = produto
+    if preco > 1000:
+        cont +=1
+    if preco < menor:
+        menor = preco
+        NomeMenor = produto
+    opcao = ' '
+    while opcao not in 'SN':
+        opcao = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if opcao in 'N':
+        break
+print('-'*17,'FIM DO PROGRAMA','-'*17)
+print(f'\nO valor total da compra foi de {total}')
+if cont > 1:
+    print(f'Temos {cont} produtos custando mais de R$1000,00')
+else:
+    print(f'Temos 1 produto custando mais de R$1000,00')
+print(f'O produto mais barato foi {NomeMenor} que custa {menor:.2f}')
+print('-'*50)
