c9382861cd8dab64a23a757a8bc2668a74b6851f
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-06 22:01:20 -0300

    Exercício 072 Finalizado

---
+++ Phython-Exercise/ex072.py
@@ -0,0 +1,7 @@
+numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    n = int(input('Digite um número:'))
+    if n >=0 and n <=20:
+        break
+    print('Tente novamente!',end=' ')
+print(f'Você digitou o número {numeros[n]}')
