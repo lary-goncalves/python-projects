1c81331e1bc07f7905d7b34c4093d5d18f797e17
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-06 22:16:08 -0300

    Exercício 072 Finalizado

--- Phython-Exercise/ex072.py
+++ Phython-Exercise/ex072.py
@@ -1,7 +1,11 @@
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
-    n = int(input('Digite um número entre 0 e 20:'))
-    if n >=0 and n <=20:
+    while True:
+        n = int(input('Digite um número entre 0 e 20:'))
+        if n >=0 and n <=20:
+            break
+        print('Tente novamente!',end=' ')
+    print(f'Você digitou o número {numeros[n]}')
+    resp=str(input('Deseja continuar? [S/N]')).strip().upper()
+    if resp in 'Nn':
         break
-    print('Tente novamente!',end=' ')
-print(f'Você digitou o número {numeros[n]}')
