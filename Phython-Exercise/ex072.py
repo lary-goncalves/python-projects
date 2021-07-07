5737e41c2d552e3b74589f5403744f63f0b841bf
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-06 22:07:57 -0300

    Exercício 072 Finalizado

--- Phython-Exercise/ex072.py
+++ Phython-Exercise/ex072.py
@@ -1,6 +1,6 @@
 numeros = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'catorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 while True:
-    n = int(input('Digite um número:'))
+    n = int(input('Digite um número entre 0 e 20:'))
     if n >=0 and n <=20:
         break
     print('Tente novamente!',end=' ')
