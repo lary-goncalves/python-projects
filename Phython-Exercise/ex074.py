7186a618d1e5eb7dede62840482799ce73ab28e7
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-14 22:31:24 -0300

    Update ex074.py

--- Phython-Exercise/ex074.py
+++ Phython-Exercise/ex074.py
@@ -1,5 +1,7 @@
 from random import randint
 numeros = (randint(0,9),randint(0,9),randint(0,9),randint(0,9),randint(0,9))
-print(f'Os valores sorteados foram: {numeros[0]} {numeros[1]} {numeros[2]} {numeros[3]} {numeros[4]}')
-print(f'O maior valor sorteado foi: {max(numeros)}')
+print(f'Os valores sorteados foram: ', end='')
+for n in numeros:
+    print(f'{n} ',end='')
+print(f'\nO maior valor sorteado foi: {max(numeros)}')
 print(f'O menor valor sorteado foi: {min(numeros)}')
