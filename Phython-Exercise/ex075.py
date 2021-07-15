b191fb0d5f8cc0c88beb8113be443368cd205fd9
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-15 00:11:34 -0300

    Update ex075.py

--- Phython-Exercise/ex075.py
+++ Phython-Exercise/ex075.py
@@ -3,12 +3,13 @@ b = int (input('Digite outro número: '))
 c = int (input('Digite mais um número: '))
 d = int (input('Digite o último número: '))
 numeros = (a, b, c, d)
+
 print (f'\nVocê digitou os números {numeros}')
 print (f'O numero 9 repetiu {numeros.count(9)} vezes')
 
 try: 
     posição = numeros.index(3)
-    print (f'O valor 3 apareceu na {numeros.index(3)}ª posição')
+    print (f'O valor 3 apareceu na {numeros.index(3)+1}ª posição')
 except ValueError:
     print('O número 3 não foi digitado')
 
