fed860159a6d8c89558765d060f054d16d6c62b1
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-14 23:29:31 -0300

    Update ex075.py

--- Phython-Exercise/ex075.py
+++ Phython-Exercise/ex075.py
@@ -3,7 +3,10 @@ b = int (input('Digite outro número: '))
 c = int (input('Digite mais um número: '))
 d = int (input('Digite o último número: '))
 numeros = (a, b, c, d)
-print (f'Você digitou os números {numeros}')
+print (f'\nVocê digitou os números {numeros}')
 print (f'O numero 9 repetiu {numeros.count(9)} vezes')
 print (f'O valor 3 apareceu na {numeros.index(3)+1}ª posição')
-print ()
+print (f'Os valores pares foram ',end='')
+for c in range(0,4):
+    if numeros[c] % 2 == 0:
+        print(f'{numeros[c]} ',end='')
