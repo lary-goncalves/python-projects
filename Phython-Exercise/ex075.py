633ab2daf9d48162ec435c2f541a7e9e42bf6aad
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-15 00:51:35 -0300

    Update ex075.py

--- Phython-Exercise/ex075.py
+++ Phython-Exercise/ex075.py
@@ -1,5 +1,4 @@
 numeros = (int (input('Digite um número: ')), int (input('Digite outro número: ')), int (input('Digite mais um número: ')), int (input('Digite o último número: ')))
-
 print (f'\nVocê digitou os números {numeros}')
 print (f'O numero 9 repetiu {numeros.count(9)} vezes')
 
@@ -10,6 +9,7 @@ except ValueError:
     print('O número 3 não foi digitado')
 
 print (f'Os valores pares foram ',end='')
+
 for c in range(0,len(numeros)):
     if numeros[c] % 2 == 0:
         print(f'{numeros[c]} ',end='')
