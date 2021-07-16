774d44e18c6160969b051915360db8e23e691da2
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-07-15 22:33:19 -0300

    Update ex076.py

--- Phython-Exercise/ex076.py
+++ Phython-Exercise/ex076.py
@@ -1,11 +1,11 @@
 print ('-'*50)
 print(f'{" LISTAGEM DE PREÇOS ": ^50}')
 print ('-'*50)
-precos = ('Lápis',1.75,'Borracha',2.00,'Caderno',15.90,'Estojo',25.00)
+precos = ('Lápis',1.75,'Borracha',2.00,'Caderno',15.90,'Estojo',25.00,'Transferidor',4.20,'Compasso',9.99,'Mochila',120.32,'Canetas',22.30,'Livros',34.90)
 
 for c in range(0,len(precos)):
     if c % 2 == 0:
-        print(f'{precos[c]:.<40}R$',end='')
+        print(f'{precos[c]:.<40}R$',end=' ')
     else:
         print(f'{precos[c]:.2f}')
 print ('-'*50)
