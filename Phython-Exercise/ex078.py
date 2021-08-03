64265d953d507bbf9356083b2a0f657647ef000c
Laryssa Goncalves <laryssag@hotmail.com.br>
2021-08-02 23:15:55 -0300

    Update ex078.py

--- Phython-Exercise/ex078.py
+++ Phython-Exercise/ex078.py
@@ -1,4 +1,15 @@
 valores = []
-for c in range (0,4):
-    valores.append() = int(input(f'Digite um número na posição {c}: '))
+for cont in range(0,5):
+    valores.append(int(input(f'Digite um número na posição {cont}: ')))
 
+print(f'Você digitou os valores {valores}')
+
+print(f'O maior valor digitado foi {max(valores)} nas posições ', end='')
+for cont in range(0,5):  
+    if max(valores) == valores[cont]:
+        print (cont,end='... ')
+
+print(f'\nO menor valor digitado foi {min(valores)} nas posições ', end='')
+for cont in range(0,5):  
+    if min(valores) == valores[cont]:
+        print (cont,end='... ')
